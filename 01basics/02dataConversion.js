2e4b990b999ec88da449d21df120bcaa940d51c8
aarush sarkari <aarushsarkari907@gmail.com>
2026-08-20 13:32:41 +0000

    next one

---
+++ 01basics/02dataConversion.js
@@ -0,0 +1,16 @@
+let score = 33
+console.log(typeof score);
+
+let valuenumber = String(score)
+console.log(typeof valuenumber);
+console.log(valuenumber);
+//"33" => 33
+//"33abc" =>NaN
+// true =>1 
+// false=> 0 
+
+// 1=> true
+// 0 => false
+
+//""= false
+// "aarush"=> true
